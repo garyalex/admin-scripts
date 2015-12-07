4e20281447fdfe6fb9bc19ec39e713a6dfc0bc4d
Gary Alexander <gary.alexander@ucs-solutions.co.za>
2015-12-07 16:08:09 +0200

    Updated watchproxy

--- watchproxy.zsh
+++ watchproxy.zsh
@@ -18,27 +18,46 @@ PROXY_FILE="/tmp/gta-squidhost"
 SSHTUNNEL_FILE="/tmp/gta-sshtunnel"
 SSH_HOST_FILE="/tmp/gta-sshtunnel.txt"
 
-# Current Proxy parent
-if [ -f "$PROXY_FILE" ]
-then
-  PROXY_PARENT="$(cat $PROXY_FILE | tr '[:lower:]' '[:upper:]')"
-else
-  PROXY_PARENT="-- NOT RUNNING? --"
-fi
+while true
+do
+  clear
+  # Current Proxy parent
+  if [ -f "$PROXY_FILE" ]
+  then
+    PROXY_PARENT="$(cat $PROXY_FILE | tr '[:lower:]' '[:upper:]')"
+  else
+    PROXY_PARENT="-- NOT RUNNING? --"
+  fi
 
-if [[ -f $SSHTUNNEL_FILE && -f $SSH_HOST_FILE ]]; then
-  SSH_HOST=$(cat $SSH_HOST_FILE)
-  SSH_STATUS="$(ssh -S $SSHTUNNEL_FILE -O check $SSH_HOST)"
-  echo "SSH HOST: $SSH_HOST STATUS: $SSH_STATUS"
-else
-  SSH_HOST="none"
-  SSH_STATUS="not running"
-  echo "SSH HOST: $SSH_HOST STATUS: $SSH_STATUS"
-fi
+  # SSH Tunnel
+  if [[ -f $SSHTUNNEL_FILE && -f $SSH_HOST_FILE ]]; then
+    SSH_HOST=$(cat $SSH_HOST_FILE)
+    SSH_STATUS="$(ssh -S $SSHTUNNEL_FILE -O check $SSH_HOST)"
+    echo "SSH HOST: $SSH_HOST STATUS: $SSH_STATUS"
+  else
+    SSH_HOST="none"
+    SSH_STATUS="not running"
+    echo "SSH HOST: $SSH_HOST STATUS: $SSH_STATUS"
+  fi
 
-echo "PROXY WATCH" | toilet -F gay -f smblock -t
-echo
-echo "$BOLD_WHITE PROXY PROFILE -$BOLD_YELLOW $PROXY_PARENT $RESET"
-echo "$BLUE=====================================$RESET"
-echo "$BOLD_CYAN SSH TUNNEL -$BOLD_GREEN $SSH_STATUS $RESET"
-echo "$BLUE=====================================$RESET"
+  # Print the status
+  echo "PROXY WATCH" | toilet -F gay -f smblock -t
+  echo
+  echo "$BOLD_WHITE PROXY PROFILE -$BOLD_YELLOW $PROXY_PARENT $RESET"
+  echo "$BLUE=====================================$RESET"
+  echo "$BOLD_CYAN SSH TUNNEL -$BOLD_GREEN $SSH_STATUS $RESET"
+  echo "$BLUE=====================================$RESET"
+  echo 
+
+  # If single mode exit, otherwise loop
+  if [[ $1 == "single" ]]; then
+    exit 0
+  else
+    sec=60
+    while [ $sec -ge 0 ]; do
+      echo -ne "COUNTDOWN TO REFRESH: $sec SECS\033[0K\r"
+      let "sec=sec-1"
+      sleep 1
+    done
+  fi
+done
