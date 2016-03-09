fab73c96820e662206b8333f412507b5e197fda3
Gary Alexander <gary.alexander@ucs-solutions.co.za>
2016-03-09 12:56:58 +0200

    Fixes and minor bugs

--- hotspot.zsh
+++ hotspot.zsh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 
 LOGS=~/logs/hotspot.log
+IFWLAN="wlp5s0"
+IFETH0="enp9s0"
 
 if [ "$#" -eq 0 ];then
         echo "AP Hotspot for phones"
@@ -11,11 +13,11 @@ else
         done       
         if [ $1 = "on" ]; then 
                 echo "Turning hostpot on"
-                sudo create_ap --daemon --no-virt -c 6 wlan0 eth0 monroeAP 9876ALEX12345 &>~/logs/hotspot.log
+                sudo create_ap --daemon --no-virt -c 6 $WLAN $ETH0 monroeAP 9876ALEX12345 &>~/logs/hotspot.log
                 echo "Use setsshtunnel to setup ssh tunnel as needed"
         elif [ $1 = "off" ]; then
                 echo "Turning hotspot off"
-                sudo create_ap --stop wlan0 
+                sudo create_ap --stop $WLAN 
                 ps aux | grep adalind | grep 8080 | awk '{ print $2; }' | xargs --no-run-if-empty kill
                 ps aux | grep hostapd | awk '{ print $2; }' | xargs --no-run-if-empty kill
                 ps aux | grep create_ap | awk '{ print $2; }' | xargs --no-run-if-empty kill
@@ -23,14 +25,14 @@ else
                 echo "Use setsshtunnel to setup ssh tunnel as needed"
         elif [ $1 = "restart" ]; then
                 echo "Turning hotspot off"
-                sudo create_ap --stop wlan0 
+                sudo create_ap --stop $WLAN 
                 ps aux | grep adalind | grep 8080 | awk '{ print $2; }' | xargs --no-run-if-empty kill
                 ps aux | grep hostapd | awk '{ print $2; }' | xargs --no-run-if-empty kill
                 ps aux | grep create_ap | awk '{ print $2; }' | xargs --no-run-if-empty kill
                 sudo create_ap --fix-unmanaged
                 echo "Turning hotspot on"
                 echo "Use setsshtunnel to setup ssh tunnel as needed"
-                sudo create_ap --daemon --no-virt -c 6 wlan0 eth0 monroeAP 9876ALEX12345 &>~/logs/hotspot.log
+                sudo create_ap --daemon --no-virt -c 6 $WLAN $ETH0 monroeAP 9876ALEX12345 &>~/logs/hotspot.log
         else
                 echo "AP Hotspot for phones"
                 echo "Usage: $0 [ on | off | restart ]"
